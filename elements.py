4fefb36488e4cdac4ea2da804feb8c3aa539a400
Qiqi Jin <ginqi7@gmail.com>
2025-08-20 21:30:36 +0800

    Chore: Format code and add type hints.

--- elements.py
+++ elements.py
@@ -1,14 +1,17 @@
 from enum import Enum, unique
 
+
 @unique
 class EditElementType(Enum):
     CLIP = "eaf-ve-clip"
     TEXT = "eaf-ve-text"
     IMAGE = "eaf-ve-image"
 
+
 class EditElement:
-    def __init__(self, type):
-        self.type = type
+    def __init__(self, type: EditElementType):
+        self.type: EditElementType = type
+
 
 class EditClip(EditElement):
     def __init__(self, file, begin, end):
@@ -16,6 +19,7 @@ class EditClip(EditElement):
         self.file = file
         self.begin = begin
         self.end = end
+
     def range_str(self):
         return f"[{self.begin} -> {self.end}]"
 
@@ -24,50 +28,52 @@ class EditClip(EditElement):
 
 
 class EditText(EditElement):
-    def __init__(self, text, duration):
+    def __init__(self, text: str, duration: int):
         EditElement.__init__(self, EditElementType.TEXT)
-        self.text = text
-        self.duration = duration
+        self.text: str = text
+        self.duration: int = duration
 
-class EditElements :
-        def __init__(self):
-            self.edit_elements = []
 
-        def get_by_type(self, type):
-            return [x for x in self.edit_elements if x.type == type]
+class EditElements:
+    def __init__(self):
+        self.edit_elements: list[EditElement] = []
 
-        def get_clips(self):
-            return self.get_by_type(EditElementType.CLIP)
+    def get_by_type(self, type: EditElementType):
+        return [x for x in self.edit_elements if x.type == type]
 
-        def get_texts(self):
-            return self.get_by_type(EditElementType.TEXT)
+    def get_clips(self):
+        return self.get_by_type(EditElementType.CLIP)
 
-        def add_clip(self, file, begin, end):
-            self.edit_elements.append(EditClip(file, begin, end))
+    def get_texts(self):
+        return self.get_by_type(EditElementType.TEXT)
 
-        def add_text(self, text, duration):
-            self.edit_elements.append(EditText(text, duration))
+    def add_clip(self, file: str, begin: int, end: int):
+        self.edit_elements.append(EditClip(file, begin, end))
 
-        def last_clip(self):
-            clips = self.get_clips()
-            if clips:
-                return clips[-1]
+    def add_text(self, text: str, duration: int):
+        self.edit_elements.append(EditText(text, duration))
 
-        def first_clip(self):
-            clips = self.get_clips()
-            if clips:
-                return clips[0]
-        def from_emacs(self, video_file, elments):
-            # [['eaf-ve-clip', [28062, 36310]], ['eaf-ve-text', ['List Delete or Install a Formula', 1]], ['eaf-ve-clip', [41344, 50448]], ['eaf-ve-clip', [54518, 59552]], ['eaf-ve-clip', [71656, 79903]], ['eaf-ve-text', ['List Installed Taps', 1]], ['eaf-ve-clip', [83116, 87294]], ['eaf-ve-text', ['Operations in Tap', 1]], ['eaf-ve-clip', [99933, 109251]], ['eaf-ve-clip', [88043, 98112]], ['eaf-ve-clip', [112571, 119319]]]
-            self.edit_elements = []
-            for element in elments:
-                type = EditElementType(element[0])
-                match type:
-                    case EditElementType.CLIP:
-                        self.add_clip(video_file, element[1][0], element[1][1])
-                    case EditElementType.TEXT:
-                        self.add_text(element[1][0], element[1][1])
-                    case EditElementType.IMAGE:
-                        print("Not Support")
-                    case _:
-                        print("Not Support")
+    def last_clip(self):
+        clips = self.get_clips()
+        if clips:
+            return clips[-1]
+
+    def first_clip(self):
+        clips = self.get_clips()
+        if clips:
+            return clips[0]
+
+    def from_emacs(self, video_file: str, elments):
+        # [['eaf-ve-clip', [28062, 36310]], ['eaf-ve-text', ['List Delete or Install a Formula', 1]], ['eaf-ve-clip', [41344, 50448]], ['eaf-ve-clip', [54518, 59552]], ['eaf-ve-clip', [71656, 79903]], ['eaf-ve-text', ['List Installed Taps', 1]], ['eaf-ve-clip', [83116, 87294]], ['eaf-ve-text', ['Operations in Tap', 1]], ['eaf-ve-clip', [99933, 109251]], ['eaf-ve-clip', [88043, 98112]], ['eaf-ve-clip', [112571, 119319]]]
+        self.edit_elements = []
+        for element in elments:
+            type = EditElementType(element[0])
+            match type:
+                case EditElementType.CLIP:
+                    self.add_clip(video_file, element[1][0], element[1][1])
+                case EditElementType.TEXT:
+                    self.add_text(element[1][0], element[1][1])
+                case EditElementType.IMAGE:
+                    print("Not Support")
+                case _:
+                    print("Not Support")
--- ffmpeg_utils.py
+++ ffmpeg_utils.py
@@ -23,61 +23,82 @@ import ffmpeg
 import os
 import elements
 
-def image_to_stream(image):
-        return ffmpeg.input(image, loop=1, t=10, framerate=30)
 
-def text_to_stream(text, time, resolution):
-        width, height = resolution
-        return ffmpeg.input(f"color=c=white:s={width}x{height}:r=30,drawtext=text='{text}':fontcolor=black:fontsize=50:x=(w-tw)/2:y=(h-th)/2",
-                            f="lavfi",
-                            t= time)
+def image_to_stream(image):
+    return ffmpeg.input(image, loop=1, t=10, framerate=30)
+
+
+def text_to_stream(text: str, time: int, resolution: int):
+    width, height = resolution
+    return ffmpeg.input(
+        f"color=c=white:s={width}x{height}:r=30,drawtext=text='{text}':fontcolor=black:fontsize=50:x=(w-tw)/2:y=(h-th)/2",
+        f="lavfi",
+        t=time,
+    )
+
 
 def export_elements_to_streams(elements, input, resolution):
-        return [export_element_to_stream(element, input, resolution) for element in elements]
+    return [
+        export_element_to_stream(element, input, resolution) for element in elements
+    ]
+
 
 def export_element_to_stream(element, input, resolution):
-        print(element)
-        if isinstance(element, elements.EditClip):
-                return clips_to_video(input, [element.begin, element.begin])
-        elif isinstance(element, elements.EditText):
-                return text_to_stream(element.text, element.duration, resolution)
+    print(element)
+    if isinstance(element, elements.EditClip):
+        return clips_to_video(input, [element.begin, element.begin])
+    elif isinstance(element, elements.EditText):
+        return text_to_stream(element.text, element.duration, resolution)
 
 
 def clips_to_video(input, clip):
-        start_time, end_time = clip[0] / 1000, clip[1] / 1000
-        return ffmpeg.input(input, ss=start_time, t=end_time - start_time)
+    start_time, end_time = clip[0] / 1000, clip[1] / 1000
+    return ffmpeg.input(input, ss=start_time, t=end_time - start_time)
+
 
 def export_streams(output_path, streams):
-        merged_stream = ffmpeg.concat(*streams)
-        # Output to new video file
-        # , v=1# , a=1
-        # v=1 indicates a video stream, and a=1 indicates an audio stream.
-        if os.path.exists(output_path):
-                os.remove(output_path)
-        output = ffmpeg.output(merged_stream, output_path)
-        ffmpeg.run(output)
+    merged_stream = ffmpeg.concat(*streams)
+    # Output to new video file
+    # , v=1# , a=1
+    # v=1 indicates a video stream, and a=1 indicates an audio stream.
+    if os.path.exists(output_path):
+        os.remove(output_path)
+    output = ffmpeg.output(merged_stream, output_path)
+    ffmpeg.run(output)
+
 
 def get_video_info(video_path):
-        try:
-                return ffmpeg.probe(video_path, show_frames=None, select_streams="v", skip_frame="nokey", of="json")
-        except Exception as e:
-                print(e.stderr)
+    try:
+        return ffmpeg.probe(
+            video_path,
+            show_frames=None,
+            select_streams="v",
+            skip_frame="nokey",
+            of="json",
+        )
+    except Exception as e:
+        print(e.stderr)
 
 
 def get_keyframe_timestamps(info):
-        frames = info['frames']
-        keyframes = [frame for frame in frames if frame.get('key_frame') == 1]
-        keyframe_timestamps = [float(frame['pts_time']) * 1000 for frame in keyframes if 'pts_time' in frame]
-        # print(keyframe_timestamps)
-        return keyframe_timestamps
+    frames = info["frames"]
+    keyframes = [frame for frame in frames if frame.get("key_frame") == 1]
+    keyframe_timestamps = [
+        float(frame["pts_time"]) * 1000 for frame in keyframes if "pts_time" in frame
+    ]
+    # print(keyframe_timestamps)
+    return keyframe_timestamps
+
 
 def get_video_resolution(info):
-        video_stream = next((stream for stream in info['streams'] if stream['codec_type'] == 'video'), None)
-        if video_stream is None:
-            print("Error: No video stream found in the file")
-            return None
-        width = video_stream['width']
-        height = video_stream['height']
+    video_stream = next(
+        (stream for stream in info["streams"] if stream["codec_type"] == "video"), None
+    )
+    if video_stream is None:
+        print("Error: No video stream found in the file")
+        return None
+    width = video_stream["width"]
+    height = video_stream["height"]
 
-        print(f"Resolution: {width}x{height}")
-        return (width, height)
+    print(f"Resolution: {width}x{height}")
+    return (width, height)
